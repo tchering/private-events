70b813cdbe578d0d9b7d431657d70942c653e820
tchering <sonam.sherpa.lama@gmail.com>
2024-10-04 19:00:56 +0200

    solved all N+1 queries

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @events = Event.includes(:participants).all
+    @events = Event.includes(:creator).all
   end
 
   def create
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,9 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
-    @events = current_user.created_events #is passed in users/show.html.erb
+    @events = @user.created_events.includes(:creator) #is passed in users/show.html.erb
     @event = Event.new #is passed in users/show.html.erb to show form error.
-    @past_events = @user.participated_events.past_events
-    @upcoming_events = @user.participated_events.upcoming_events
+    @past_events = @user.participated_events.past_events.includes(:creator)
+    @upcoming_events = @user.participated_events.upcoming_events.includes(:creator)
   end
 end
