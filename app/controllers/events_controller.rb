22b22d9ca73ab59b052aa8f825e0636c1d0fc9b1
tchering <sonam.sherpa.lama@gmail.com>
2024-10-05 12:29:35 +0200

    feat: created new branch to test different routes -Modified routes to make it more readable and simpler -Added routes for join and leaving events -Updated views to use new path helpers -Modified EventsController to handle joining and leaving events logic -Deleted UserEvents controller since it was redundant -Corrected all the N+1 queries problems

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,7 +23,7 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
     if @event.destroy
       flash[:notice] = "The event #{@event.name} has been deleted"
-      redirect_to user_path(current)
+      redirect_to user_path(current_user)
     else
       flash[:notice] = "Sorry event cannot be deleted"
       redirect_back
@@ -32,6 +32,34 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+  end
+
+  def join
+    @user = current_user
+    @event = Event.find(params[:id])
+    @join_event = UserEvent.new(user: @user, event: @event)
+    if @event.creator == current_user
+      flash[:notice] = "You cant join your own event"
+    elsif UserEvent.exists?(user_id: current_user.id, event_id: @event.id)
+      flash[:notice] = "You have already joined this event"
+    elsif @join_event.valid?
+      @join_event.save
+      flash[:success] = "You have joined event #{@event.name}"
+    else
+      flash[:alert] = "Unable to join the event"
+    end
+    redirect_to event_path(@event)
+  end
+
+  def leave
+    @event = Event.find(params[:id])
+    @join_event = UserEvent.find_by(user_id: current_user.id, event_id: @event.id)
+    if @join_event&.destroy
+      redirect_to event_path(@event)
+    else
+      flash[:notice] = "Unable to leave this event "
+      redirect_to event_path(@event)
+    end
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = current_user
-    @events = @user.created_events.includes(:creator) #is passed in users/show.html.erb
+    @events = @user.created_events #is passed in users/show.html.erb
     @event = Event.new #is passed in users/show.html.erb to show form error.
     @past_events = @user.participated_events.past_events.includes(:creator)
     @upcoming_events = @user.participated_events.upcoming_events.includes(:creator)
